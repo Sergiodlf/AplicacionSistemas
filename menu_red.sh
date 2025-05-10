4e55bcb8d232df4b31ebb2a538bf2a1ee9f4b9e9
Sergiodlf <sergio4vientos@gmail.com>
2025-05-10 13:41:25 +0200

    Menú de funcionalidades de red

---
+++ menu_red.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+while true; do
+        opcion=4;
+        opcion=$(zenity --list --title="🖥 Menú de funcionalidades de red"  \
+        --column="Opción" --column="Descripción" \
+        1 "Mostrar dirección IP" \
+        2 "Medir velocidad de red" \
+        3 "Verificar conectividad a Internet" \
+        4 "Salir" \
+        --width=400 --height=300)
+
+    case $opcion in
+        1)
+            ip_addr=$(hostname -I | awk '{print $1}')
+            zenity --info --title="Dirección IP" --text="Tu dirección IP es: $ip_addr"
+            ;;
+        2)
+            if ! command -v speedtest-cli &> /dev/null; then
+                zenity --error --title="Error" --text="speedtest-cli no está instalado. Instálalo con:\nsudo apt install speedtest-cli"
+            else
+                speed_output=$(speedtest-cli --simple)
+                zenity --info --title="Velocidad de red" --text="$speed_output"
+            fi
+            ;;
+        3)
+            if ping -c 1 8.8.8.8 &> /dev/null; then
+                zenity --info --title="Conectividad" --text="¡Tienes conexión a Internet!"
+            else
+                zenity --error --title="Conectividad" --text="No hay conexión a Internet."
+            fi
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
